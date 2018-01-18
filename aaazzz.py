ec49e258f9466bfae00872fbdc6f4513e99b1f73
Jake Cordelli <jakecordelli12345@gmail.com>
2018-01-18 10:22:47 -0500

    Update aaazzz.py
    added mapping

--- aaazzz.py
+++ aaazzz.py
@@ -1,12 +1,19 @@
 '''This program is designed to create a dictionary of values ranging from AAA-ZZZ'''
 
 
-'''Generate a list of values from AAA-ZZZ'''
+'''Generate a list of strings from AAA-ZZZ'''
 import string
 ltrs=list(string.ascii_lowercase)
 '''x holds the list we need'''
 x=[''.join([a,b,c]) for a in ltrs for b in ltrs for c in ltrs]
-print(x)
+#print(x)
 
 
-'''Map these values to a dictionary'''
+'''Map these strings to values in a dictionary'''
+dictionary={}
+
+for i in range(17576):
+    dictionary[x[i]]=i
+
+print (dictionary)
+
